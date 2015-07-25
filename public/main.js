f2fc979e71417d5a9b77655e14fa13530e145073
seansaysmeh@gmail.com <seansaysmeh@gmail.com>
2015-07-25 15:58:38 +0100

    added deletion of complete items

--- public/main.js
+++ public/main.js
@@ -3,6 +3,9 @@ var todoListPlaceholder = document.getElementById("todo-list-placeholder");
 var form = document.getElementById("todo-form");
 var todoTitle = document.getElementById("new-todo");
 var error = document.getElementById("error");
+var count = document.getElementById("count-label");
+var deleteCompletedButton = document.getElementById("delete-completed");
+var todos = [];
 
 form.onsubmit = function(event) {
     var title = todoTitle.value;
@@ -42,17 +45,46 @@ function getTodoList(callback) {
     createRequest.send();
 }
 
+function deleteCompleted() {
+    var completedTodos = [];
+    todos.forEach(function (todo) {
+        if (todo.isComplete) {
+            completedTodos.push(todo);
+        }
+    });
+    var doneSoFar = completedTodos.length;
+    function allDone() {
+        doneSoFar = doneSoFar - 1;
+        if (doneSoFar === 0) {
+            reloadTodoList();
+        }
+    }
+    completedTodos.forEach(function (todo) {
+        deleteTodo(todo, allDone);
+    });
+}
+
 function reloadTodoList() {
+    var self = this;
     while (todoList.firstChild) {
         todoList.removeChild(todoList.firstChild);
     }
     todoListPlaceholder.style.display = "block";
     getTodoList(function(todos) {
+        self.todos = todos;
         todoListPlaceholder.style.display = "none";
+        var uncompleteCount = 0;
         todos.forEach(function(todo) {
             todoList.appendChild(createTodoElement(todo));
+            if (!todo.isComplete) {
+                uncompleteCount++;
+            }
         });
+        count.textContent = "Items left to complete : " + uncompleteCount;
     });
+    deleteCompletedButton.onclick = function() {
+        deleteCompleted();
+    };
 }
 
 function createTodoElement(todo) {
@@ -65,19 +97,19 @@ function createTodoElement(todo) {
     itemLabel.onkeydown = function(event) {
         if (event.keyCode === 13) {
             todo.title = itemLabel.value;
-            updateTodo(todo);
+            updateTodo(todo, reloadTodoList);
         }
     };
     var deleteButton = document.createElement("button");
     deleteButton.textContent = "delete";
     deleteButton.onclick = function() {
-        deleteTodo(todo);
+        deleteTodo(todo, reloadTodoList);
     };
     var completeButton = document.createElement("button");
     completeButton.textContent = "complete";
     completeButton.onclick = function() {
         todo.isComplete = !todo.isComplete;
-        updateTodo(todo);
+        updateTodo(todo, reloadTodoList);
     };
     listItem.appendChild(completeButton);
     listItem.appendChild(deleteButton);
@@ -85,7 +117,7 @@ function createTodoElement(todo) {
     return listItem;
 }
 
-function updateTodo(todo) {
+function updateTodo(todo, success) {
     var createRequest = new XMLHttpRequest();
     createRequest.open("PUT", "/api/todo");
     createRequest.setRequestHeader("Content-type", "application/json");
@@ -93,19 +125,19 @@ function updateTodo(todo) {
     createRequest.send(JSON.stringify(todo));
     createRequest.onload = function() {
         if (this.status === 200) {
-            reloadTodoList();
+            success();
         } else {
             error.textContent = "Failed to update item. Server returned " + this.status + " - " + this.responseText;
         }
     };
 }
 
-function deleteTodo(todo) {
+function deleteTodo(todo, success) {
     var createRequest = new XMLHttpRequest();
     createRequest.open("DELETE", "/api/todo/" + todo.id);
     createRequest.onload = function() {
         if (this.status === 200) {
-            reloadTodoList();
+            success();
         } else {
             error.textContent = "Failed to delete todo. Server returned " + this.status + " - " + this.responseText;
         }
